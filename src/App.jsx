9adbf057be856730e908c2cb3d452451cb739701
Matias Ezequiel Diaz <diaz.me1984@gmail.com>
2024-01-07 22:06:59 -0300

    import and return FeedVideos element

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
+import FeedVideos from './components/FeedVideos/FeedVideos'
 
 function App() {
   return (
     <div className='App'>
       <main>
-        TiktokClon
+        <FeedVideos/>
       </main>
     </div>
   )

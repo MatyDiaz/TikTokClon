bee017d7efb123f323f0ddc19c52e111aecd951f
Matias Ezequiel Diaz <diaz.me1984@gmail.com>
2024-01-14 15:36:50 -0300

    Add handlers in VideoPlayerActions component

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,18 +1,34 @@
 import { Heart } from "../Icons/Heart";
+import { Comment } from "../Icons/Comment";
+import { Share } from "../Icons/Share";
 import style from './VideoPlayer.module.css';
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({likes = 12, comments = 35, shares = 25}) {
+
+    const handleLike = () => {
+        alert('Like!')
+    };
+    const handleComment = () => {
+        alert('Comment!')
+    };
+    const handleShare = () => {
+        alert('Share!')
+    };
+
     return (
         <aside className={style.actions}>
-            <div className={style.action}>
-                <Heart></Heart>
-            </div>
-            <div className={style.action}>
-                <Heart></Heart>
-            </div>
-            <div className={style.action}>
-                <Heart></Heart>
-            </div>
+            <button onClick={handleLike} className={style.action}>
+                <Heart/>
+                <span title='like' >{likes}</span>
+            </button>
+            <button onClick={handleComment} className={style.action}>
+                <Comment/>
+                <span title='comment' >{comments}</span>
+            </button>
+            <button onClick={handleShare} className={style.action}>
+                <Share/>
+                <span title='shares' >{shares}</span>
+            </button>
         </aside>
     )
 }
